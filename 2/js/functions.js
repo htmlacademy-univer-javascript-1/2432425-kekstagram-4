0a5ebd1df8800a892b64c828d6bb3f72182753b8
Keks <keks@htmlacademy.ru>
2023-10-05 13:14:29 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/functions.js
@@ -0,0 +1,22 @@
+function checkLength(str, length) {
+  if (str.length <= length){return true;}
+  else {return false;}
+}
+
+checkLength('проверяемая строка', 20);
+checkLength('проверяемая строка', 18);
+checkLength('проверяемая строка', 10);
+
+function checkPalindrome(str) {
+  str = str.toLowerCase().replaceAll(' ', '');
+  for (let i = 0; i < str.length / 2; i++) {
+    const j = str.length - 1 - i;
+    if (str[i] !== str[j]) {return false;}
+  }
+  return true;
+}
+
+checkPalindrome('топот');
+checkPalindrome('ДовОд');
+checkPalindrome('Кекс');
+checkPalindrome('Лёша на полке клопа нашёл ');
